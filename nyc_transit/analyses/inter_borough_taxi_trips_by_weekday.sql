6ed8ab2ab0a6044d2f8bfaff4e2b0f329a2a4edd
Lee Ge <yge2@andrew.cmu.edu>
2023-07-29 21:24:35 -0400

    This is the end of Week 4

--- nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
+++ nyc_transit/analyses/inter_borough_taxi_trips_by_weekday.sql
@@ -1,33 +1,23 @@
-select
-    diff.Weekday,
-    trips_different_borough,
-    trips_total,
-    CONCAT(ROUND(trips_different_borough /trips_total * 100, 2), '%') AS Percentage_trips_different_borough
-from
-(
-  select
-      DAYOFWEEK(pickup_datetime) as Weekday,
-      count(*) as trips_different_borough
-  from mart__fact_all_taxi_trips a
-  left join mart__dim_locations b
-  on a.pulocationid = b.locationid
-  left join mart__dim_locations c
-  on a.dolocationid = c.locationid
-where b.borough <> c.borough
-group by DAYOFWEEK(pickup_datetime) ) diff
+with all_trips as
+(select
+    weekday(pickup_datetime) as weekday,
+    count(*) trips
+    from {{ ref('mart__fact_all_taxi_trips') }} t
+    group by all),
 
-join
-(
-  select
-      DAYOFWEEK(pickup_datetime) as Weekday,
-      count(*) as trips_total
-  from mart__fact_all_taxi_trips a
-  left join mart__dim_locations b
-  on a.pulocationid = b.locationid
-  left join mart__dim_locations c
-  on a.dolocationid = c.locationid
-  group by DAYOFWEEK(pickup_datetime) ) total
+inter_borough as
+(select
+    weekday(pickup_datetime) as weekday,
+    count(*) as trips
+from {{ ref('mart__fact_all_taxi_trips') }} t
+join {{ ref('mart__dim_locations') }} pl on t.PUlocationID = pl.LocationID
+join {{ ref('mart__dim_locations') }} dl on t.DOlocationID = dl.LocationID
+where pl.borough != dl.borough
+group by all)
 
-on diff.Weekday = total.Weekday
-ORDER BY diff.Weekday;
-
+select all_trips.weekday,
+       all_trips.trips as all_trips,
+       inter_borough.trips as inter_borough_trips,
+       inter_borough.trips / all_trips.trips as percent_inter_borough
+from all_trips
+join inter_borough on (all_trips.weekday = inter_borough.weekday);
